2cfb062d5aa5fe165316504fde7fb281a4b892a0
mjbiener <michaelbiener@yahoo.com>
2021-02-09 19:26:29 -0500

    search results populate image/artist/song on page

--- main.js
+++ main.js
@@ -1,33 +1,62 @@
 console.log ("hello")
 
 const url='https://itunes.apple.com/search?term='
-const form =document.querySelector('#music-search')
+const proxyUrl= 'https://proxy-itunes-api.glitch.me/search?term='
+const form =document.querySelector('#music-search-form')
 const songList= document.querySelector('#song-list')
 
 form.addEventListener('submit', function (event) {
-    console.log('running')
+    // console.log('running')
     event.preventDefault()
     let searchInput = document.querySelector('#search-box').value
-    // const musicInfo = document.querySelector('#search-box').value
-    searchRequest()
+    searchRequest(searchInput)
 })
-
-
 
 function searchRequest () {
     let searchInput = document.querySelector('.search').value
     fetch(url + searchInput)
     .then(res => res.json())
     .then(data => {
-        // for (let music of data) {
-            console.log(data)
-            // renderMusicItem(music)
-        })
-    }    
-// }
-// searchRequest()
-// function renderMusicItem () {
+        for (let song of data.results) {
+            console.log(song.trackName) //.trackName  .artistName //
+            renderMusicResults(song)
+        }
+    })   
+}
+
+function renderMusicResults (song) {
+    let songData = document.createElement ('div')
+    let indSong = document.createElement('li')
+    
+    let artwork = document.createElement('img')
+    artwork.src=song.artworkUrl100
+    indSong.appendChild(artwork)
+    
+    let title = document.createElement('p')
+    title.innerText = song.trackName
+    indSong.appendChild(title)
+        
+    let artistName= document.createElement('H4')
+    artistName.innerText=song.artistName
+    indSong.appendChild(artistName)
+    songList.appendChild(indSong)
+}
+
+    // musicEl.appendChild(title)
+    // songList.appendChild(musicEl)
+    
+    // renderMusicInformation(musicEl, musicObj)
+    // songList.appendChild(musicEl)
+
+
+
+// function renderMusicInformation (songListItem, musicObj) {
+//     console.log(musicObj)
+//     songListItem.innerHTML = `<p>${musicObj.trackName}<p>`
+    // songListItem.innerHTML = `<p>${musicObj.artistName}<p>`
 
 // }
+
+
 
 // listMusic()
